40ebf7f99e062fc6dc5fe20f55c7ee84ded1a415
Patrick Munnich <38660244+munnich@users.noreply.github.com>
2022-12-15 13:08:36 +0100

    Update examineSegment.m

--- src/examineSegment.m
+++ src/examineSegment.m
@@ -2,14 +2,16 @@ function isHit = examineSegment(fftMagnitude, segment, rest)
     low = rest(1);
     high = rest(2);
     % normalized bandpass
-    slicedFft = normalize.(fftMagnitude(low:high));
+    slicedFft = normalize(fftMagnitude(low:high));
     % scale the segment
     scaledSegment = zeros(length(segment));
     for i = 1:length(segment)
         scaledSegment(i) = segment(i) - low;
     end
     % we have to make sure the rest segment isn't greater than the whole length
-    sliceMean = mean(slicedFft(scaledSegment(1):min(scaledSegment(2), end)));
+    limitA = scaledSegment(1);
+    limitB = min(scaledSegment(2), length(slicedFft));
+    sliceMean = mean(slicedFft(limitA:limitB));
     if segment(2) == length(slicedFft)
         sliceMeanRest = mean(slicedFft(1:scaledSegment(1)));
     elseif segment(1) == 1
