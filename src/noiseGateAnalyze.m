b85681b7ca95997f27a99d89579304b882504d18
Patrick Munnich <38660244+munnich@users.noreply.github.com>
2023-01-11 17:25:34 +0100

    add some documentation

--- src/noiseGateAnalyze.m
+++ src/noiseGateAnalyze.m
@@ -9,8 +9,18 @@ function isAbove = noiseGateAnalyze(audio, threshold)
     %
     % Returns:
     % * isAbove: 0 for no values in audio above threshold, 1 otherwise
-    isAbove = 0;
+    isAbove = 0; % the default output if nothing is detected should be 0
+    % for our noise gate we check if any sample within the audio array is
+    % above our threshold
     if any(audio > threshold)
+        % if this is the case we return one
         isAbove = 1;
     end
+    % in case we'd want to support an additional block to detect e.g. a
+    % correct pronunciation we could include another block here which sets
+    % isAbove = -1;
+    % for example:
+    % if any(audio < threshold)
+    %     isAbove = -1;
+    % end
 end
--- src/noiseGateCalibrate.m
+++ src/noiseGateCalibrate.m
@@ -7,11 +7,18 @@ function threshold = noiseGateCalibrate(In)
     % * In: input audio device
     % 
     % Returns:
-    % * threshold: noise gate threshold; 4 * maximum of 1s audio recording
+    % * threshold: noise gate threshold; 4 * maximum of 2s audio recording
 
-    In.SamplesPerFrame = In.SampleRate;
-
+    disp("Please remain silent for 3 seconds.") % instructions
+    % we're not actually doing 3 seconds but we need a buffer for reading
+    pause(0.5); % sleep for a second before recording - to remove e.g. key presses
+    In.SamplesPerFrame = In.SampleRate * 2; % 2 second recording
+    
+    % start saving the audio array
     audio = step(In);
-
+    disp("Done.")
+    % set our analysis parameter for the noise gate
+    % this is just the maximum of the audio array multiplied by an
+    % arbitrary number
     threshold = max(audio) * 4;
 end
